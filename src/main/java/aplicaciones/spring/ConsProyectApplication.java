3bc7396f8e9eef3704d08f8cf85e6eb7d3bf751d
YesicaLope <yesica.lope@tecsup.edu.pe>
2021-11-24 23:07:18 -0500

    Archivos subidos

---
+++ src/main/java/aplicaciones/spring/ConsProyectApplication.java
@@ -0,0 +1,13 @@
+package aplicaciones.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsProyectApplication.class, args);
+	}
+
+}
